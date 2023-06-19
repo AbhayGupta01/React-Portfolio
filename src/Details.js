faecec3db91e6e33cf86d95dace6139183ccb00d
AbhayGupta01 <abhaygupta083@gmail.com>
2023-06-19 23:49:48 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,179 @@
+// Enter all your detials in this file
+// Logo images
+// import logogradient from "./assets/logo.svg";
+// import logo from "./assets/logo2.svg";
+import icons from "./assets/icons.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+// import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+// import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+// import projectImage4 from "./assets/projects/project4.jpg";
+// import projectImage5 from "./assets/projects/project5.jpg";
+// import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logo: icons,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Abhay Gupta",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Hi, I'm Abhay, Web Developer. Passionate about building websites and solving algorithmic problems. Based in Uttrakhand.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/abhay-gupta-88b405209/",
+  github: "https://github.com/AbhayGupta01",
+  twitter: "https://twitter.com/abhay_gupta_01",
+  instagram: "https://www.instagram.com/abhay.g01/",
+};
+
+// Enter your Work Experience here
+// export const workDetails = [
+//   {
+//     Position: "Salesforce",
+//     Company: `PWC`,
+//     Location: "Bengaluru",
+//     Type: "Full Time",
+//     Duration: "Sep 2021 - Dec 2021",
+//   },
+//   {
+//     Position: "Internship",
+//     Company: `Company Name here`,
+//     Location: "Bengaluru",
+//     Type: "Internship",
+//     Duration: "Sep 2021 - Dec 2021",
+//   },
+//   {
+//     Position: "Internship",
+//     Company: `Company Name here`,
+//     Location: "Bengaluru",
+//     Type: "Internship",
+//     Duration: "Sep 2021 - Dec 2021",
+//   },
+// ];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "B.Tech Computer Science & Engineering",
+    Company: "VIT Bhopal University",
+    Location: "Sehore, Madhya Pradesh",
+    Type: "Ongoing",
+    Duration: "Sept 2020 - Present",
+  },
+  {
+    Position: "Senior Secondary School",
+    Company: `Maria Assumpta Convent School`,
+    Location: "Kashipur, Uttarakhand",
+    Type: "Completed",
+    Duration: "Aug 2020",
+  },
+  {
+    Position: "High School",
+    Company: `Maria Assumpta Convent School`,
+    Location: "Kashipur, Uttarakhand",
+    Type: "Completed",
+    Duration: "May 2018",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Stat Tracker",
+    image: projectImage1,
+    description: `Monitor and analyze the statistics of your preferred FPS games, and gain insights into 
+    the amount of time you have devoted to them`,
+    techstack: "MongoDB, ExpressJS, ReactJS, NodeJS, Material-UI, Steam_Web_API",
+    // previewLink: "https://google.com",
+    githubLink: "https://github.com/AbhayGupta01/Game-Stat-Tracker",
+  },
+  {
+    title: "Recollections",
+    image: projectImage2,
+    description: `Recollection of beautiful memories of different places visited by people`,
+    techstack: " MongoDB, ExpressJS, ReactJS, NodeJS, Redux, Material-UI",
+    // previewLink: "https://google.com",
+    githubLink: "https://github.com/AbhayGupta01/Recollections",
+  },
+  {
+    title: "Growth View",
+    image: projectImage3,
+    description: `React-based admin dashboard application that provides detailed analysis of employees 
+    and customers for businesses`,
+    techstack: "ReactJs, Syncfusion, Tailwind CSS, Material-UI",
+    // previewLink: "https://google.com",
+    githubLink: "https://github.com/AbhayGupta01/GrowthView",
+  },
+//   {
+//     title: "Project title 4",
+//     image: projectImage4,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 5",
+//     image: projectImage5,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 6",
+//     image: projectImage6,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "abhaygupta083@gmail.com",
+  phone: "+91 7017698854",
+};
